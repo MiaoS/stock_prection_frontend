534b2097621515e49a5e87dadefdb632a3db4ed9
MiaoS <miaoshi2013@gmail.com>
2017-12-12 00:47:11 -0800

    minor fix

--- routes/stock.js
+++ routes/stock.js
@@ -12,7 +12,7 @@ router.get('/:symbol', function(req, res, next) {
   	if(err){
   		res.json(err);
   	}else{
-      item.type =req.query.type||"binary";
+      item.type = sreq.query.type||"binary";
       if(item.type=="binary"){
   		  res.render("stock",item);
       }else{
